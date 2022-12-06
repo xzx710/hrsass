881ebc6dab567d11da4787c0d27252bbadcfa1f2
xiezexuan <2974758363@qq.com>
2022-12-06 18:16:20 +0800

    ‘登录模块完成’

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,28 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+
 const state = {
+  token: getToken()
 }
 const mutations = {
+  // 设置token
+  setToken(state, token) {
+    state.token = token // 设置token  只是修改state的数据  123 =》 1234
+    // vuex变化 => 缓存数据
+    setToken(token) // vuex和 缓存数据的同步
+  },
+  // 删除缓存
+  removeToken(state) {
+    state.token = null // 删除vuex的token
+    removeToken() // 先清除 vuex  再清除缓存 vuex和 缓存数据的同步
+  }
 }
 
 const actions = {
+  async login(context, data) {
+    const result = await login(data)
+    context.commit('setToken', result)
+  }
 }
 
 export default {
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,28 @@
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import axios from 'axios'
-const service = axios.create() // 创建一个axios的实例
+import { Message } from 'element-ui'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+}) // 创建一个axios的实例
 service.interceptors.request.use() // 请求拦截器
-service.interceptors.response.use() // 响应拦截器
+
+// 响应拦截器
+service.interceptors.response.use(response => {
+  // axios默认加了一层data
+  const { success, message, data } = response.data
+  //   要根据success的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    // 业务已经错误了 还能进then ? 不能 ！ 应该进catch
+    Message.error(message) // 提示错误消息
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message) // 提示错误信息
+  return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
+})
+
 export default service // 导出axios实例
